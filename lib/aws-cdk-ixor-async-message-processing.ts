4588c9d46d7273ca57dc76558a84d4c6a6e77444
Rik Tytgat <rik.tytgat@tryx.com>
2020-09-23 10:28:45 +0200

    Add support for pattern with SNS and subscription filters

--- lib/aws-cdk-ixor-async-message-processing.ts
+++ lib/aws-cdk-ixor-async-message-processing.ts
@@ -1,7 +1,7 @@
-import {Aws, Construct, StackProps,Duration} from "@aws-cdk/core";
+import {Construct, StackProps,Duration} from "@aws-cdk/core";
 import {IFunction} from "@aws-cdk/aws-lambda";
-import {Queue, DeadLetterQueue} from "@aws-cdk/aws-sqs";
-import {ITopic, Topic} from "@aws-cdk/aws-sns";
+import {Queue} from "@aws-cdk/aws-sqs";
+import {ITopic, SubscriptionFilter, Topic} from "@aws-cdk/aws-sns";
 import {LambdaSubscription, SqsSubscription} from "@aws-cdk/aws-sns-subscriptions";
 import {SqsEventSource} from "@aws-cdk/aws-lambda-event-sources";
 
@@ -11,42 +11,76 @@ export interface AsyncMessageProcessingProps extends StackProps {
   sqsProperties?: SqsProperties
 }
 
+export interface SqsSnsSubscriber {
+  name: string,
+  sqsConsumer: IFunction,
+  sqsProperties?: SqsProperties,
+  filterPolicy?: {[p: string]: SubscriptionFilter}
+}
+
 interface SqsProperties {
   visibilityTimeout?: Duration,
   lambdaEventSourceBatchSize?: number;
 }
 
+// noinspection JSUnusedGlobalSymbols
 export class AsyncMessageProcessing extends Construct {
   public snsTopic: ITopic;
   private maxReceiveCount: number = 5;
   private lambdaEventSourceBatchSize: number;
 
-  constructor(scope: Construct, id: string, props: AsyncMessageProcessingProps) {
+  // noinspection JSUnusedGlobalSymbols
+  constructor(scope: Construct, id: string, props?: AsyncMessageProcessingProps) {
     super(scope, id);
 
-    this.getLambdaEventSourceBatchSize(props);
+    this.snsTopic = new Topic(this, 'topic', {});
 
-    const queueDLQ = new Queue(this, 'deadLetterQueue', {});
-    const dlqProperties: DeadLetterQueue = {queue: queueDLQ, maxReceiveCount: this.maxReceiveCount}
-    const queueMain = new Queue(this, 'mainQueue', {
+    if (props) {
+      this.getLambdaEventSourceBatchSize(props);
+
+      const queueDLQ = new Queue(this, 'deadLetterQueue', {});
+      const queueMain = new Queue(this, 'mainQueue', {
+        deadLetterQueue: {
+          queue: queueDLQ,
+          maxReceiveCount: this.maxReceiveCount
+        },
+        visibilityTimeout: props.sqsProperties?.visibilityTimeout || Duration.seconds(30),
+      });
+
+      this.snsTopic.addSubscription(new SqsSubscription(queueMain, {
+        filterPolicy: {
+          "action": new SubscriptionFilter([{"exists": false}])
+        }
+      }));
+
+      if (props.snsLambdaSubscribers) {
+        props.snsLambdaSubscribers.forEach(lambda => {
+          this.snsTopic.addSubscription(new LambdaSubscription(lambda));
+        });
+      }
+
+      props.sqsConsumer.addEventSource(new SqsEventSource(queueMain, {batchSize: this.lambdaEventSourceBatchSize}));
+    }
+  }
+
+  // noinspection JSUnusedGlobalSymbols
+  public addSqsSnsSubscriber(sqsSnsSubscriber: SqsSnsSubscriber) {
+    const queueDLQ = new Queue(this, `dlq${sqsSnsSubscriber.name}`, {});
+    const queue = new Queue(this, `q${sqsSnsSubscriber.name}`, {
       deadLetterQueue: {
         queue: queueDLQ,
         maxReceiveCount: this.maxReceiveCount
       },
-      visibilityTimeout: props.sqsProperties?.visibilityTimeout || Duration.seconds(30),
-
+      visibilityTimeout: sqsSnsSubscriber.sqsProperties?.visibilityTimeout || Duration.seconds(30)
     });
 
-    this.snsTopic = new Topic(this, 'topic', {});
-    this.snsTopic.addSubscription(new SqsSubscription(queueMain));
-
-    if (props.snsLambdaSubscribers) {
-      props.snsLambdaSubscribers.forEach(lambda => {
-        this.snsTopic.addSubscription(new LambdaSubscription(lambda));
-      });
-    }
-
-    props.sqsConsumer.addEventSource(new SqsEventSource(queueMain, {batchSize: this.lambdaEventSourceBatchSize}));
+    this.getLambdaEventSourceBatchSize(sqsSnsSubscriber);
+    this.snsTopic.addSubscription(new SqsSubscription(queue, {
+      filterPolicy: sqsSnsSubscriber.filterPolicy
+    }));
+    sqsSnsSubscriber.sqsConsumer.addEventSource(new SqsEventSource(queue, {
+      batchSize: this.lambdaEventSourceBatchSize
+    }));
   }
 
   private getLambdaEventSourceBatchSize(props: any) {
